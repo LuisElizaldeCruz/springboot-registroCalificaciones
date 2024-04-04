fda9708a9e1916478f9124a5cce71290e2a5b8f1
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2024-04-03 23:35:27 -0600

    primer commit

---
+++ src/test/java/com/luis/proyectoRegCalificaciones/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis.proyectoRegCalificaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
